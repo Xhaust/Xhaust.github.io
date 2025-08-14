377b1ad7b6b21fdb0eb6a3bc872eb1e598f7ed4e
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-08-14 16:30:01 +0200

    Add hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+export const HeroSection = () => {
+    return (
+    <section id="hero">
+        <div>
+            <h1 className="text=4xl md: text-6xl font-bold">
+                <span>Hi I'm</span>
+                <span> Pol</span>
+                <span> Labrador</span>
+            </h1>
+        </div>
+    </section>
+
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import { HeroSection } from "../components/HeroSection"
+
 export const Home = () => {
-    return <div>Home</div>
+    return (
+        <main>
+            <HeroSection />
+        </main>
+    )
 }
