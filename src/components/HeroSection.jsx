4b4e48372f80195a8160eb8b2bce72540cc15e07
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-08-16 12:10:06 +0200

    Appliy simple formatting to Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,14 @@
 export const HeroSection = () => {
     return (
-    <section id="hero">
-        <div>
-            <h1 className="text=4xl md: text-6xl font-bold">
-                <span>Hi I'm</span>
-                <span> Pol</span>
-                <span> Labrador</span>
-            </h1>
+    <section id="hero" className="relative min-h-screen flex items-center justify-center px-4">
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className="space-y-6">
+                <h1 className="text=4xl md: text-6xl font-bold tracking-tight">
+                    <span> Hi I'm</span>
+                    <span> Pol</span>
+                    <span> Labrador</span>
+                </h1>
+            </div>
         </div>
     </section>
 
