f051d544d0d74a72ac687e9b6ce09e7b175f99bc
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-08-18 23:34:34 +0200

    Add icons to ThemeToggle + check localStorage for theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,11 +1,39 @@
+import { Sun, Moon } from 'lucide-react';
+import { useState, useEffect} from 'react';
+
 export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme === "dark") {
+            setIsDarkMode(true)
+            document.documentElement.classList.add('dark');
+        } else {
+            localStorage.theme = "light";
+            setIsDarkMode(false);
+        }
+    }, []);
+
     const toggleTheme = () => {
-        document.documentElement.classList.toggle('dark');
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = "light";
+            setIsDarkMode(false);
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.theme = "dark";
+            setIsDarkMode(true);
+        }
     };
-    
+
     return (
-        <button onClick={toggleTheme} className="dark">
-        Toggle Theme
+        <button onClick={toggleTheme} >
+            {isDarkMode ? (
+                <Sun className="w-6 h-6 text-yellow-300" />
+                ) : (
+                <Moon className="w-6 h-6 text-blue-900" />
+            )}
         </button>
     );
-    }
+};  
