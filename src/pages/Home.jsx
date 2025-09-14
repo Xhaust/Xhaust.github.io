e380131e68044fd60179a2f9eda2713d52a1c7d2
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-09-14 20:31:07 +0200

    add projects component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "../components/HeroSection"
 import { ThemeToggle } from "../components/ThemeToggle"
+import { Projects } from "../components/Projects"
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground">
             <ThemeToggle />
             <HeroSection />
+            <Projects />
         </div>
     )
 }
