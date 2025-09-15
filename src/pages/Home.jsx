16e36f298db678dc4ede3ae3e0af01dc1032b964
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-09-15 18:14:17 +0200

    style: rename projects section file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { HeroSection } from "../components/HeroSection"
 import { ThemeToggle } from "../components/ThemeToggle"
-import { Projects } from "../components/Projects"
+import { ProjectsSection } from "../components/ProjectsSection"
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground">
             <ThemeToggle />
             <HeroSection />
-            <Projects />
+            <ProjectsSection/>
         </div>
     )
 }
