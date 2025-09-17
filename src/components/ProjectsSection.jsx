16d2eb2d07786a7f534d7df962f65c0dcbb15ab0
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-09-17 23:37:47 +0200

    Add VB calculator app

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,16 +1,48 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Simple Calculator",
+        description: "This is a simple calculator app built with Visual Basic. It performs basic arithmetic operations like addition, subtraction, multiplication, and division.",
+        tags: ["Visual Basic"],
+        image: "projects/calculator.png",
+        githubLink: "https://github.com/Xhaust/VB-Simple-Calculator"
+    }
+]
+
 export const ProjectsSection = () => {
     return (
-        <section id="projects" className="relative min-h-screen flex items-center justify-center px-4">
-            <div className="container max-w-4xl mx-auto text-center z-10">
-                <div className="space-y-6">
-                    <h2 className="text-4xl md:text-6xl font-bold tracking-tight">
-                        <span className="text-primary">Projects</span>
-                    </h2>
-                    <p className="text-lg md:text-xl text-muted-foreground max-2-xl mx-auto">
-                        Here are some of my recent works.
-                    </p>
-                </div>
+        <section id="projects" className="py-24 px-4 relative">
+        <div className="container mx-auto max-w-5xl">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                Featured <span className="text-primary">Projects </span>    
+            </h2> 
+        
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                Here are somo of my recent works. Most of these projects are open-source and available on my GitHub.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {projects.map((project, key) => (
+                    <div key={key} className="group rounded-lg overflow-hidden shadow-xs card-hover">
+                        <div className= "h-48 overflow-hidden">
+                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transform group-hover:scale-105 transition duration-300"/>
+                        </div>
+                        <div className="p-6"> 
+                            <div className= "flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary" key={tag}>{tag}</span>
+                                ))}
+                            </div>
+                        </div>
+                        <h3 className="text-xl font-semibold mb-2 px-6">{project.title}</h3>
+                        <p className="text-gray-600 mb-4 px-6">{project.description}</p>
+                        <div className="px-6">
+                            <a href={project.githubLink} className="text-primary font-semibold">View on GitHub</a>
+                        </div>
+                    </div>
+                ))}
             </div>
+        </div>
         </section>
     )
 }
