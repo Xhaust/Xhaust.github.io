b20e629728983cde0346028a5d4c2a1f2190c314
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-10-04 12:50:12 +0200

    refactor(themetoggle): remake theme handling for readability

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,35 +1,25 @@
-import { Sun, Moon } from 'lucide-react';
-import { useState, useEffect} from 'react';
+import { Sun, Moon } from "lucide-react";
+import { useState, useEffect} from "react";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [theme, setTheme] = useState("dark");
 
     useEffect(() => {
-        const storedTheme = localStorage.getItem('theme');
-        if (storedTheme === "dark") {
-            setIsDarkMode(true)
-            document.documentElement.classList.add('dark');
-        } else {
-            localStorage.theme = "light";
-            setIsDarkMode(false);
-        }
+        const storedTheme = localStorage.getItem("theme") || "dark";
+        setTheme(storedTheme);
+        document.documentElement.classList.toggle("dark", storedTheme === "dark");
     }, []);
 
     const toggleTheme = () => {
-        if (isDarkMode) {
-            document.documentElement.classList.remove('dark');
-            localStorage.theme = "light";
-            setIsDarkMode(false);
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.theme = "dark";
-            setIsDarkMode(true);
-        }
+        const newTheme = theme === "dark" ? "light" : "dark";
+        setTheme(newTheme);
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
+        localStorage.setItem("theme", newTheme);
     };
 
     return (
         <button onClick={toggleTheme} className="fixed top-5 z-50 right-5 rounded-full" >
-            {isDarkMode ? (
+            {theme === "dark" ? (
                 <Sun className="w-6 h-6 text-yellow-300" />
                 ) : (
                 <Moon className="w-6 h-6 text-blue-900" />
