d53faf5c16675a6131cc60ada1a74afed7fa1e10
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-10-09 22:40:35 +0200

    feat: add pulsating stars to the background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { HeroSection } from "../components/HeroSection"
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
 import { ProjectsSection } from "../components/ProjectsSection"
 import { NavBar } from "../components/NavBar"
 import { ExperienceSection } from "../components/ExperienceSection"
@@ -8,6 +9,7 @@ export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground">
             <ThemeToggle />
+            <StarBackground />
             <NavBar />
             <HeroSection />
             <ExperienceSection />
